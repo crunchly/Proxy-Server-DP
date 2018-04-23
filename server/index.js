22616f236e731e61ab4b5218b091b5a711f6479c
seungprk@gmail.com <seungprk@gmail.com>
2018-04-23 14:38:29 -0700

    Route chart API to the chart server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
@@ -7,6 +8,13 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Dynamic
+app.use('/funding-rounds', (req, res) => {
+  fetch(`http://localhost:3001${req.originalUrl}`)
+    .then(response => response.json())
+    .then(data => res.send(data));
+});
 
 app.listen(port, () => {
   process.stdout.write(`server running at: http://localhost:${port}\n`);
