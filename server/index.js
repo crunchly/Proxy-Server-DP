17df042f9f96bce4dd5bab47e0d104c9f4863bb0
seungprk@gmail.com <seungprk@gmail.com>
2018-04-23 12:14:41 -0700

    Setup simple server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => {
+  process.stdout.write(`server running at: http://localhost:${port}\n`);
+});
