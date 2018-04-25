9ba16a985a3b63a5310c37403fb3db9931b5a5c6
seungprk@gmail.com <seungprk@gmail.com>
2018-04-25 10:27:23 -0700

    Fix further lint issues with console output

--- server/index.js
+++ server/index.js
@@ -14,36 +14,28 @@ app.use('/funding-rounds', (req, res) => {
   fetch(`http://localhost:3001${req.originalUrl}`)
     .then(fetchRes => fetchRes.json())
     .then(data => res.send(data))
-    .catch(() => {
-      res.sendStatus(404);
-    });
+    .catch(() => res.sendStatus(404));
 });
 
 app.use('/overview', (req, res) => {
   fetch(`http://localhost:3002${req.originalUrl}`)
     .then(response => response.json())
     .then(data => res.send(data))
-    .catch(() => {
-      res.sendStatus(404);
-    });
+    .catch(() => res.sendStatus(404));
 });
 
 app.use('/people', (req, res) => {
   fetch(`http://localhost:3004${req.originalUrl}`)
     .then(response => response.json())
     .then(data => res.send(data))
-    .catch(() => {
-      res.sendStatus(404);
-    });
+    .catch(() => res.sendStatus(404));
 });
 
 app.use('/api/funding_round', (req, res) => {
   fetch(`http://localhost:3003${req.originalUrl}`)
     .then(response => response.json())
     .then(data => res.send(data))
-    .catch(() => {
-      res.sendStatus(404);
-    });
+    .catch(() => res.sendStatus(404));
 });
 
 app.listen(port, () => {
