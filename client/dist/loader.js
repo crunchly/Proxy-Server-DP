be33c5f2a1c567bf44140be3f379ef74cfd83c73
seungprk@gmail.com <seungprk@gmail.com>
2018-04-27 15:35:51 -0700

    Make company name dynamic and add current team module

--- client/dist/loader.js
+++ client/dist/loader.js
@@ -2,6 +2,7 @@ window.onload = () => {
   const modules = [
     { uri: 'http://ec2-13-57-175-222.us-west-1.compute.amazonaws.com', css: '/style.css' },
     { uri: 'http://ec2-13-59-188-143.us-east-2.compute.amazonaws.com', css: '/style.css' },
+    { uri: 'http://ec2-52-34-86-177.us-west-2.compute.amazonaws.com', css: '/styles.css' },
   ];
 
   modules.forEach((module) => {
@@ -15,4 +16,8 @@ window.onload = () => {
     link.rel = 'stylesheet';
     document.getElementsByTagName('head')[0].appendChild(link);
   });
+
+  const { pathname } = window.location;
+  const company = pathname.slice(pathname.lastIndexOf('/', pathname.length - 2) + 1, -1);
+  document.getElementsByClassName('content-head__company')[0].textContent = company;
 };
