94795e3ea7c892c144dae8a76558cdf3a99b1193
seungprk@gmail.com <seungprk@gmail.com>
2018-04-28 11:28:10 -0700

    Fix test description typo

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,5 +6,5 @@ jest.mock('node-fetch');
 
 test('get "/" should send redirect', () => request(app).get('/').expect(302));
 test('get "/organization/Facebook/funding-rounds" should return data', () => request(app).get('/organization/Facebook/funding-rounds').expect(dummyData));
-test('get /overview should return data', () => request(app).get('/people/Facebook').expect(dummyData));
+test('get /people should return data', () => request(app).get('/people/Facebook').expect(dummyData));
 test('get /api/funding_round should return data', () => request(app).get('/api/funding_round').expect(dummyData));
