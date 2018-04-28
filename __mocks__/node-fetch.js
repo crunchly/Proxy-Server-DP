1e73a7db9ba73a2ea514743aef58a3cd69308e5d
seungprk@gmail.com <seungprk@gmail.com>
2018-04-27 20:38:41 -0700

    Complete tests

--- __mocks__/node-fetch.js
+++ __mocks__/node-fetch.js
@@ -1,3 +1,21 @@
-const fetch = (url);
+const dummyData = require('./dummyData.js');
+
+const dummyResponse = {
+  json: () => dummyData,
+};
+
+const fetch = url => (
+  new Promise((resolve, reject) => {
+    if (
+      url === 'http://ec2-13-57-175-222.us-west-1.compute.amazonaws.com/funding-rounds/Facebook' ||
+      url === 'http://ec2-52-34-86-177.us-west-2.compute.amazonaws.com/people/Facebook' ||
+      url === 'http://ec2-13-59-188-143.us-east-2.compute.amazonaws.com/api/funding_round'
+    ) {
+      resolve(dummyResponse);
+    } else {
+      reject(new Error());
+    }
+  })
+);
 
 module.exports = fetch;
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,10 @@
 const request = require('supertest');
 const app = require('../server/app');
+const dummyData = require('../__mocks__/dummyData');
 
 jest.mock('node-fetch');
 
 test('get "/" should send redirect', () => request(app).get('/').expect(302));
-test('get /organization/:company/funding-rounds should return data', () => );
-test('get /overview should return data', () => true);
-test('get /api/funding_round should return data', () => true);
+test('get "/organization/Facebook/funding-rounds" should return data', () => request(app).get('/organization/Facebook/funding-rounds').expect(dummyData));
+test('get /overview should return data', () => request(app).get('/people/Facebook').expect(dummyData));
+test('get /api/funding_round should return data', () => request(app).get('/api/funding_round').expect(dummyData));
